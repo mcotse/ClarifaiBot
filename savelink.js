05becc10fdcf6d8a9896474361827b49990a76b5
Matthew Tse <mtse9779@gmail.com>
2016-05-18 23:20:53 -0400

    fitted with localhost

--- savelink.js
+++ savelink.js
@@ -1,11 +1,16 @@
 'use strict'
-const crypto = require('crypto')
-const request = require('request')
-const config = require('./config')
-var firebase = require('firebase');
+// const crypto = require('crypto')
+// const request = require('request')
+// const config = require('./config')
+// var firebase = require('firebase');
 
-var app = firebase.initializeApp(config.firebase_config);
+// var app = firebase.initializeApp(config.firebase_config);
 
-var createNewLink(uid, link) => {
-  
+let uid = '1231232123141'
+
+function createNewLink(uid, link){
+  firebase.database().ref('links/' + Date.now()).push({
+    link: link,
+    uid: uid
+  })
 }
