df1276457ebd9c233a75c0f77940b202e8d00ff3
Matthew Tse <mtse9779@gmail.com>
2016-10-14 11:27:02 -0400

    config/gitignore added

---
+++ config.example.js
@@ -0,0 +1,17 @@
+//messenger-bot
+const id = 'FB-MSGER-ID'
+const secret = 'FB-MSGER-SECRET'
+
+//imgur
+const user = 'IMGUR-ID'
+const pw = 'IMGUR-PW'
+
+//firebase
+const firebase_config = {
+    apiKey: "API",
+    authDomain: "domain",
+    databaseURL: "url",
+    storageBucket: "bucket",
+  };
+
+module.exports = { id, secret, user, pw, firebase_config}
